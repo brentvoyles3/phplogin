c684fd096df18aafedc078bca64d643111a0d468
Brent.Voyles3 <Brent.Voyles3@gmail.com>
2024-09-17 10:46:54 -0400

    Added css to login form

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div id="form">
+        <h1> Login </h1>
+        <form name="form" method="POST">
+            <label>Username: </label>
+            <input type="text" id="user" name="user"></input>
+            <br> <br>
+            <label>Password: </label>
+            <input type="passsword" id="pass" name="pass"></input>
+            <br> <br>
+            <input type="submit" id="btn" value="Submit" name="Submit"/>
+        </form>
+        
+</div>
+</body>
+</html>
